57c2443d55bfa52f0bfe30de2a6ff33c3de7b3d3
GuryanovaE <GuryanovaE@DOM201.LOCAL>
2021-10-01 12:13:18 +0700

    Анимация кролей

---
+++ app/src/main/java/com/example/animew/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.animew;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView img = (ImageView)findViewById(R.id.imageView);
+        img.setBackgroundResource(R.drawable.rabbit_animation);
+        AnimationDrawable frameAnimation = (AnimationDrawable) img.getBackground();
+        frameAnimation.setOneShot(false);
+        frameAnimation.start();
+
+    }
+}
